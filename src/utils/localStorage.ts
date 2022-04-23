84c5ee278168f689d9c9a27718a5e29ca5413959
Anton <anton_marusin@mail.ru>
2022-04-24 02:01:04 +1000

    (R-1) Refinement of the sidebar.

---
+++ src/utils/localStorage.ts
@@ -0,0 +1,10 @@
+export const getLocalStorage = (key: string) => {
+  if (typeof window !== "undefined") {
+    return window.localStorage.getItem(key);    
+  }
+  return undefined;
+};
+
+export const setLocalStorage = (key: string, value: string) => {
+  window.localStorage.setItem(key, value);
+}
