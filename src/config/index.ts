4303b20d5b78e868460d98b5951478faf5377099
Anton Marusin <50703769+Toha4@users.noreply.github.com>
2024-01-20 17:47:13 +1000

    (R-47) Fix refresh token

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,2 +1,3 @@
 export const API_URL = process.env.NEXT_PUBLIC_API_URL;
+export const NEXT_INSTANCE_URL = process.env.NEXT_PUBLIC_URL;
 export const ACCESS_TOKEN_LIFETIME_SEC = process.env.NEXT_PUBLIC_ACCESS_TOKEN_LIFETIME_SEC;
--- src/utils/axiosApi.ts
+++ src/utils/axiosApi.ts
@@ -3,26 +3,27 @@ import createAuthRefreshInterceptor from "axios-auth-refresh";
 import { API_URL } from "../config";
 import { setAccessCookie } from "./cookies";
 
-
 // Create axios instance.
 const axiosInstance = axios.create({
   baseURL: API_URL,
   withCredentials: true,
 });
 
-
 // Function that will be called to refresh authorization
-const refreshAuthLogic = (failedRequest: any) => axiosInstance.get('http://localhost:3000/api/auth/refresh').then(resp => {
-  // Set up new access token
-  const { access } = resp.data;  
-  setAccessCookie(access);
+const refreshAuthLogic = (failedRequest: any) =>
+  axios
+    .get(`/api/auth/refresh`, { headers: failedRequest.response.headers })
+    .then((resp) => {
+      // Set up new access token
+      const { access } = resp.data;
+      setAccessCookie(access);
 
-  // Set up access token of the failed request.
-  failedRequest.response.config.headers.Authorization = `Bearer ${access}`;
+      // Set up access token of the failed request.
+      failedRequest.response.config.headers.Authorization = `Bearer ${access}`;
 
-  // Retry the request with new setup!
-  return Promise.resolve();
-});
+      // Retry the request with new setup!
+      return Promise.resolve();
+    });
 
 // Create axios interceptor
 createAuthRefreshInterceptor(axiosInstance, refreshAuthLogic);
