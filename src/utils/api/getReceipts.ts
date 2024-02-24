a8e8a8ef59b9a0c92ef242e754347151fd79dcb9
Anton Marusin <50703769+Toha4@users.noreply.github.com>
2024-02-24 16:50:11 +1000

    (R-68) Add settings "Proverka checka" integration

--- src/utils/api/getReceipts.ts
+++ src/utils/api/getReceipts.ts
@@ -3,7 +3,7 @@ import { ReceiptsTypes } from "./types";
 
 export const GetReceiptsApi = (instance: AxiosInstance) => ({
   async getReceiptByQrRow(qrRaw: string) {
-    const { data } = await instance.get<ReceiptsTypes>("/api/get_receipts/get_receipts_by_qrraw/", {
+    const { data } = await instance.get<ReceiptsTypes>("/api/proverka_cheka/proverka_cheka_by_qrraw/", {
       params: { qr_raw: qrRaw },
     });
     return data;
